b6c26d11def82cee0284c3cd729904d30710e529
Aluno Manhã <priscillapedrette@gmail.com>
2024-05-23 20:27:38 -0300

    V.1

---
+++ main.py
@@ -0,0 +1,9 @@
+# lista
+nomes = ['Dirce', 'Amanda', 'Eduardo', 'Rodrigo', 'Priscilla']
+
+# ordenando a lista
+nomes.sort(reverse=True)
+
+# exibir a lista na tela
+for nome in nomes:
+    print(nome)
